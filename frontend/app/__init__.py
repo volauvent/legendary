80f757f30dd302358416a09cccffe01c6c8380e6
vol <xiabingfeng@gmail.com>
2017-03-17 11:39:39 -0400

    finish prediction flask API

--- frontend/app/__init__.py
+++ frontend/app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+import os
 
 app = Flask(__name__)
 
 app.config.from_object('config')
 db = SQLAlchemy(app)
 Bootstrap(app)
+
+UPLOAD_FOLDER = os.path.abspath('.') + '/upload'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 from app import views, models
--- frontend/app/views.py
+++ frontend/app/views.py
@@ -7,6 +7,10 @@ import socket
 import pickle
 import numpy as np
 import random
+from werkzeug import secure_filename
+
+
+ALLOWED_EXTENSIONS=set(['png', 'jpg', 'jpeg', 'gif'])
 
 
 @app.route('/')
@@ -21,15 +25,8 @@ def labelling():
     return render_template('labelling.html', title='Labelling')
 
 
-@app.route('/imageLable', methods=['POST', 'GET'])
-def imageLable():
-    if request.mothod == 'POST':
-        # add a new label of given picture into the database
-        return
-    else:
-        # send back un-labeled/week-labeled picture
-        # as well as the week-labeled
-        return
+def allowedFile(filename):
+    return '.' in filename and filename.rsplit('.',1)[1] in ALLOWED_EXTENSIONS
 
 
 @app.route('/imgClassify', methods=['POST', 'GET'])
@@ -38,8 +35,45 @@ def imgClassify():
         # take the upload picture as an input
         # carry out emotion classification
         # return emotion category
-        return
+
+        if 'image-up' not in request.files:  # no image
+            print("no image!")
+            return jsonify(message='Network problem, no image...')
+
+        fileMeta = request.files["image-up"]
+        # print(fileMeta)
+        filename = fileMeta.filename
+        # print(filename)
+        if fileMeta and allowedFile(filename):
+            filename = secure_filename(filename)
+            # print("")
+            # print(filename)
+            # print(app.config['UPLOAD_FOLDER'])
+            # print("")
+            fileMeta.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            print("saved!")
+
+            ################
+            # add prediction logic
+            ################
+
+            return jsonify(message='success', predictResult='guess what? hahaha')
+        else:
+            print("File format error!")
+            return jsonify(message='File format error!')
+
     else:
         # take the choosen emotion category as an input
         # return example picture of this emotion
+        return
+
+
+@app.route('/imageLable', methods=['POST', 'GET'])
+def imageLable():
+    if request.mothod == 'POST':
+        # add a new label of given picture into the database
+        return
+    else:
+        # send back un-labeled/week-labeled picture
+        # as well as the week-labeled
         return
