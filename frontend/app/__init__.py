4f67e80b5a7f50567528b12a677106d637158d49
vol <xiabingfeng@gmail.com>
2017-03-18 11:58:24 -0400

    finish predict and labelling flask func

--- frontend/app/__init__.py
+++ frontend/app/__init__.py
@@ -6,10 +6,12 @@ import os
 app = Flask(__name__)
 
 app.config.from_object('config')
-db = SQLAlchemy(app)
+#db = SQLAlchemy(app)
 Bootstrap(app)
 
 UPLOAD_FOLDER = os.path.abspath('.') + '/upload'
+REQUEST_FOLDER = '/static/reqImg'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['REQUEST_FOLDER'] = REQUEST_FOLDER
 
 from app import views, models
--- frontend/app/views.py
+++ frontend/app/views.py
@@ -1,5 +1,5 @@
 from flask import jsonify, render_template, session, url_for, request, g, abort
-from app import app, db
+from app import app#, db
 import json
 from sqlalchemy import and_
 import os
@@ -22,6 +22,7 @@ def index():
 @app.route('/')
 @app.route('/labelling')
 def labelling():
+    # if request.method == 'GET':
     return render_template('labelling.html', title='Labelling')
 
 
@@ -35,33 +36,31 @@ def imgClassify():
         # take the upload picture as an input
         # carry out emotion classification
         # return emotion category
-
         if 'image-up' not in request.files:  # no image
             print("no image!")
             return jsonify(message='Network problem, no image...')
-
         fileMeta = request.files["image-up"]
-        # print(fileMeta)
         filename = fileMeta.filename
-        # print(filename)
         if fileMeta and allowedFile(filename):
             filename = secure_filename(filename)
-            # print("")
-            # print(filename)
-            # print(app.config['UPLOAD_FOLDER'])
-            # print("")
+            
             fileMeta.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            print("saved!")
+            emotionCategory = 'guess what?'
 
             ################
             # add prediction logic
             ################
+            # -->call Classification Model get EMOTION_CATEGORY
+            # -->call DB Model store image
 
-            return jsonify(message='success', predictResult='guess what? hahaha')
+            # image store folder path: UPLOAD_FOLDER in __init__.py
+            # predict result: emotionCategory(string)
+
+            
+            return jsonify(message='success', predictResult=emotionCategory)
         else:
             print("File format error!")
             return jsonify(message='File format error!')
-
     else:
         # take the choosen emotion category as an input
         # return example picture of this emotion
@@ -70,10 +69,29 @@ def imgClassify():
 
 @app.route('/imageLable', methods=['POST', 'GET'])
 def imageLable():
-    if request.mothod == 'POST':
+    if request.method == 'POST':
         # add a new label of given picture into the database
-        return
+        newLable = request.form.getlist("label[]")
+        if len(newLable) > 0:
+            #print("New label: ", newLable)
+            ################
+            # add store new label logic
+            ################
+            # newLable = ['NEW_EMOTION']
+
+            return jsonify(message='success')
+        else:
+            return jsonify(message='empty')
     else:
         # send back un-labeled/week-labeled picture
         # as well as the week-labeled
-        return
+
+        imageName = random.randint(0, 4)
+        reqImage = app.config['REQUEST_FOLDER'] + "/" + str(imageName) + ".jpg"
+        weekLabel = ['awe', 'contentment', 'anger']
+        ################
+        # add request image and label logic
+        ################
+        # -->call DB server get img file path and weeklabel
+
+        return jsonify(img=reqImage, label=weekLabel)
