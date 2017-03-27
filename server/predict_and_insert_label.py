5e7c36e6acfc77954785d4b6f386aa43a448bdae
vol <xiabingfeng@gmail.com>
2017-03-27 15:49:14 -0400

    add pred and insert py

---
+++ server/predict_and_insert_label.py
@@ -0,0 +1,15 @@
+from os import listdir
+import sys
+import os
+from os.path import isfile, join
+sys.path.append(os.path.abspath('./'))
+from server.baseClient import dbClient
+client = dbClient()
+mypath = "frontend/upload"
+onlyfiles = [f for f in listdir(mypath) if (
+    isfile(join(mypath, f)) and ('.jpg' in f or '.png' in f))]
+
+for f in onlyfiles:
+    # print(f)
+    print(f+str(client.predict_and_insert(f)))
+print("Done")
